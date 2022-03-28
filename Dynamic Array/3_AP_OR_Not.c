8466d6add8b8db9e067d2a03f4f6759d202e98f4
Mr Rajesh <34417363+rajeshrmsrup@users.noreply.github.com>
2022-03-28 23:04:51 +0530

    Add files via upload

---
+++ Dynamic Array/3_AP_OR_Not.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+void ap(int *ar, int n){
+    int i, d, flg=0;
+    d = ar[1] - ar[0];
+    for(i=1; i<n-1; i++){
+        if(d == ar[i+1]-ar[i]){
+            d = ar[i+1]-ar[i];
+            flg=0;
+        }
+        else{
+            flg++;
+            break;
+        }
+    }
+    if(flg == 0){
+        for(i=0; i<n; i++){
+            printf("%d\t", ar[i]);
+        }
+        printf("is AP.");
+    }
+    else{
+        for(i=0; i<n; i++){
+            printf("%d\t", ar[i]);
+        }
+        printf("Series is not AP.");
+    }
+}
+int main(){
+    int i, n;
+    printf("Enter the total no. of elements: ");
+    scanf("%d", &n);
+
+    int *ptr = (int*)malloc(n*sizeof(int));
+
+    for(i=0; i<n; i++)
+        scanf("%d", &ptr[i]);
+    ap(ptr, n);
+}
