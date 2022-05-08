70b77cb22ea327a69ebf6aaa2f0e4224054e6ae5
Mr Rajesh <34417363+rajeshrmsrup@users.noreply.github.com>
2022-05-08 20:00:24 +0530

    Add files via upload

---
+++ Linked List/DoublyLinkedList.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct DoublyLL{
+    struct DoublyLL *prv;
+    int info;
+    struct DoublyLL *next;
+}node;
+//Function Declarations..
+void insert(node **, node **, int);
+void displayForward(node *);
+void displayReverse(node *);
+//Main function
+void main(){
+    node *left=NULL, *right=NULL;
+    int ch, num;
+    do{
+        printf("\n---Menu---\n1. Insert\n2. Display in forward order\n3. Display in Reverse order\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+        case 1:
+            printf("Enter an element: ");
+            scanf("%d", &num);
+            insert(&left, &right, num);
+            break;
+        case 2:
+            displayForward(left);
+            break;
+        case 3:
+            displayReverse(right);
+        }
+    }while(ch <= 3);
+}
+void insert(node **left, node **right, int num){
+    node *p=NULL;
+    p=(node*)malloc(sizeof(node));
+    if(p == NULL)
+        printf("\tCan't insert, bcoz don't have enough memory\n");
+    else{
+        p->info = num;
+        if(*right == NULL){
+            *left=p;
+            *right=p;
+        }
+        else if(num > (*right)->info){
+            (*right)->next = p;
+            p->prv = *right;
+            *right = p;
+        }
+        else if(num < (*left)->info){
+            p->next=*left;
+            (*left)->prv = p;
+            *left = p;
+
+        }
+        else{
+            node *tmp = *left;
+            while(num > (tmp->next)->info){
+                tmp = tmp->next;
+            }
+            (tmp->next)->prv = p;
+            p->next = tmp->next;
+            tmp->next = p;
+            p->prv = tmp;
+        }
+        (*left)->prv=NULL;
+        (*right)->next = NULL;
+    }
+}
+void displayForward(node *left){
+    if(left == NULL)
+        printf("\tLinked list is Empty\n");
+    else
+        while(left !=  NULL){
+            printf("\t%d", left->info);
+            left = left->next;
+        }
+}
+void displayReverse(node *right){
+    if(right == NULL)
+        printf("\tLinked list is Empty\n");
+    else
+        while(right !=  NULL){
+            printf("\t%d", right->info);
+            right = right->prv;
+        }
+}
